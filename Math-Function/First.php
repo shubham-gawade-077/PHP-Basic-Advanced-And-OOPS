ef73c8e38455be243ae556fdf9ec648fde07f13a
Shubham Gawade <160474162+shubham-gawade-077@users.noreply.github.com>
2025-03-01 13:19:55 +0530

    Add files via upload

---
+++ Math-Function/First.php
@@ -0,0 +1,12 @@
+<?php
+echo pi()."<br>";
+echo min(10,30,50)."<br>";
+echo max(10,30,50)."<br>";
+echo abs(-4.54)."<br>";
+echo sqrt(64)."<br>";
+echo rand()."<br>";
+echo rand(1,10)."<br>";
+echo round(0.60)."<br>";
+echo round(0.49)."<br>";
+
+?>
