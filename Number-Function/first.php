ed8a8c02e5332dfc20770a36da92dd612b1977b4
Shubham Gawade <160474162+shubham-gawade-077@users.noreply.github.com>
2025-03-01 13:19:35 +0530

    Add files via upload

---
+++ Number-Function/first.php
@@ -0,0 +1,14 @@
+<?php
+$num=10;
+echo is_int($num)."<br>";
+
+$num1=10.22;
+echo is_float($num)."<br>";
+
+$x = acos(8);
+var_dump($x)."<br>";
+echo is_nan($x)."<br>";
+
+$num2="66";
+echo is_numeric($num2)."<br>";
+?>
